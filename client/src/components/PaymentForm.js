595df04523020ba41dc4fa3d79d95721330dd8bf
Samantha Laine <samanthalaine8@gmail.com>
2021-10-11 19:34:41 -0400

    project

--- client/src/components/PaymentForm.js
+++ client/src/components/PaymentForm.js
@@ -52,7 +52,7 @@ const PaymentForm = ({ checkoutToken, nextStep, backStep, shippingData, onCaptur
             <div style={{ display: 'flex', justifyContent: 'space-between' }}>
               <Button variant="outlined" onClick={backStep}>Back</Button>
               <Button type="submit" variant="contained" disabled={!stripe} style={{ backgroundColor: "#00ACC1", color: '#FAFAFA' }}>
-                Pay 
+                Pay {checkoutToken.live.subtotal.formatted_with_symbol}
               </Button>
             </div>
           </form>
